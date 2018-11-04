99652f35540b3ff9c1a2bc5c1bc06ee9b1481832
Abi Raskin <abiraskin@gmail.com>
2018-11-04 03:45:28 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   low_key = nil
   low_value = nil
-  name_hash.collect do |key, value|
+  name_hash.each do |key, value|
     if low_key == nil && v < low_value
       low_value = v
       low_key = k
